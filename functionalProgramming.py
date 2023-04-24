f9ad4fb72d4dbba3a1abe8bdc25331c157e6d1ef
diodoarrahman <130964202+diodoarrahman@users.noreply.github.com>
2023-04-24 15:48:43 +0700

    Create functionalProgramming.py

---
+++ functionalProgramming.py
@@ -0,0 +1,13 @@
+def jarak(v, t):
+    s = v * t
+    return s
+
+v1 = 5
+t1 = 3
+s1 = jarak(v1, t1)
+print("Kecepatan =", v1, "\nWaktu =", t1, "\nJarak =", s1)
+
+v2 = 7
+t2 = 5
+s2 = jarak(v2, t2)
+print("\nKecepatan =", v2, "\nWaktu =", t2, "\nJarak =", s2)
